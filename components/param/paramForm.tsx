17d5bdbdf0cce1b6f6ab8918f4558819548a502d
jmatondo <j_matondo@hotmail.com>
2025-01-28 01:48:52 +0100

    ss

--- components/param/paramForm.tsx
+++ components/param/paramForm.tsx
@@ -15,15 +15,19 @@ const ParamForm = ({ params }: ParamFormProps) => {
         <Link href="/">Home</Link>
         <Link href="/simulIPR">Simulation</Link>
       </div>
-      <p className="text-white">Parameters</p>
-      {params.map((param: any) => (
-        <div key={param.id} className="p-4 bg-[#212025]">
-          <span className="text-neutral-400">
-            {param.label}- {param.value}
-          </span>
-          <Label className="p-2 bg-[#cae6eb] text-black ">{param.label}</Label>
-        </div>
-      ))}
+      <p className="text-white">Para meters</p>
+      {params
+        ? params.map((param: any) => (
+            <div key={param.id} className="p-4 bg-[#212025]">
+              <span className="text-neutral-400">
+                {param.label}- {param.value}
+              </span>
+              <Label className="p-2 bg-[#cae6eb] text-black ">
+                {param.label}
+              </Label>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
